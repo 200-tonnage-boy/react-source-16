380c8f5db2e0f0a7e231f2c4bfdadb001aeb0fec
cjy-dell <554951030@qq.com>
2021-12-03 01:06:07 +0800

    1

---
+++ src/react/.eslintrc.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const {
+  es5Paths,
+  esNextPaths,
+} = require('./scripts/shared/pathsByLanguageVersion');
+
+const restrictedGlobals = require('confusing-browser-globals');
+
+const OFF = 0;
+const ERROR = 2;
+
+module.exports = {
+ 
+};
